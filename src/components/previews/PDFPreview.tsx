55294832b0eefd4803bf7c74b104d97d9c5d8226
wilyw520 <126368138+wilyw520@users.noreply.github.com>
2023-04-30 17:29:17 +0800

    Update PDFPreview.tsx

--- src/components/previews/PDFPreview.tsx
+++ src/components/previews/PDFPreview.tsx
@@ -11,7 +11,7 @@ const PDFEmbedPreview: React.FC<{ file: any }> = ({ file }) => {
   const pdfPath = encodeURIComponent(
     `${getBaseUrl()}/api/raw/?path=${asPath}${hashedToken ? `&odpt=${hashedToken}` : ''}`
   )
-  const url = `https://mozilla.github.io/pdf.js/web/viewer.html?file=${pdfPath}`
+  const url = `http://pdf.wilyw.top/web/viewer.html?file=${pdfPath}`
 
   return (
     <div>
