0022c1be52e0b8b69c939372d7f06214cc90eae6
wilyw520 <126368138+wilyw520@users.noreply.github.com>
2023-04-30 15:07:48 +0800

    Add files via upload

--- config/site.config.js
+++ config/site.config.js
@@ -38,11 +38,11 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Copyright © 2023 <a href="https://share.wilyw.top/" target="_blank" rel="noopener noreferrer">Wilyw Share</a> | Powered by <a href="https://blog.wilyw.top/" target="_blank" rel="noopener noreferrer">Wilyw</a> ',
+    'Copyright © 2023 <a href="https://share.wilyw.top/" target="_blank" rel="noopener noreferrer">Wilyw Share</a>|Powered by <a href="https://blog.wilyw.top/" target="_blank" rel="noopener noreferrer">Wilyw</a>. ',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
+  protectedRoutes: ['/归路','/向风而行','/以家人之名','/请叫我总监'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:mail.wilyw.top',
